d583265dd80e441923f8c297a9bad37e5f25a717
killian-dent <64610812+killian-dent@users.noreply.github.com>
2020-09-24 15:48:57 -0600

    add clinical vitals ddl

---
+++ ddl/raw/ssm/vitals.sql
@@ -0,0 +1,11 @@
+create table ssm.vitals (
+    patient_id varchar(255),
+    patient_account_number varchar(255),
+    clinical_encounter_id varchar(255),
+    vitals_captured_by varchar(255),
+    vitals_name varchar(255),
+    vitals_value numeric(18,2),
+    vitals_unit varchar(255),
+    created_timestamp timestamp,
+    modified_timestamp timestamp
+);
