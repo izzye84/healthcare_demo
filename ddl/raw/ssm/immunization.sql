3c2bbe114bcf2e4c88aa10c8565dd8c06223d1e8
killian-dent <64610812+killian-dent@users.noreply.github.com>
2020-09-24 16:49:46 -0600

    add clinical immunization ddl

---
+++ ddl/raw/ssm/immunization.sql
@@ -0,0 +1,20 @@
+create table ssm.immunization (
+    patient_id varchar(255),
+    patient_account_number varchar(255),
+    clinical_encounter_id varchar(255),
+    given_date timestamp,
+    immunization_description varchar(255),
+    lot_number varchar(255),
+    vaccine_manufacturer varchar(255),
+    immunization_dose numeric(18,2),
+    site varchar(255),
+    route varchar(255),
+    expiration_date timestamp,
+    cvx_code varchar(255),
+    cpt_code varchar(255),
+    immunization_given_by_id varchar(255),
+    immunization_date timestamp,
+    vfc_status varchar(255),
+    created_timestamp timestamp,
+    modified_timestamp timestamp
+);
