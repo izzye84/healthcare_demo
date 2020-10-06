c55441d815139a91dddff56666ede0cd1406ee41
Killian Dent <killian.dent@gmail.com>
2020-10-06 22:02:43 +0000

    Create coverage resoure

--- models/staging/ssm/base/base_ssm__eligibility.sql
+++ models/staging/ssm/base/base_ssm__eligibility.sql
@@ -1,39 +1,34 @@
 {{
-  config(
-    enabled = false
-  )
+    config(
+        materialized = 'ephemeral'
+    )
 }}
 
 with 
 
 eligibility as (
-    select trim(patient_id) as patient_id
+    select row_number() over(partition by patient_id,lob,insurance_name order by ingest_date desc, eff_date desc) as row_num
+      ,trim(patient_id) as patient_id
       ,trim(lob) as lob
       ,trim(insurance_name) as insurance_name
       ,eff_date
       ,term_date
-      ,patient_id
       ,relation
-      ,lob
-      ,insurance_name
       ,client_id
       ,ingest_date 
     from {{ source('ssm_claims','member_eligibility') }}
-),
-
-final as (
-    select distinct
-      {{ dbt_utils.surrogate_key(['patient_id','lob','insurance_name','eff_date']) }} as identifier
-      ,{{ dbt_utils.surrogate_key(['patient_id','lob','insurance_name']) }} as identifier_strive_id
-      ,eff_date as period_coverage_start
-      ,term_date as period_coverage_end
-      ,patient_id as subscriber_id
-      ,relation as relationship
-      ,lob as network
-      ,insurance_name as payor
-      ,client_id
-      ,ingest_date
-    from eligibility
 )
 
-select * from final
+select distinct
+  {{ dbt_utils.surrogate_key(['patient_id','lob','insurance_name','eff_date']) }} as identifier
+  ,{{ dbt_utils.surrogate_key(['patient_id','lob','insurance_name']) }} as identifier_strive_id
+  ,eff_date as period_coverage_start
+  ,term_date as period_coverage_end
+  ,{{ empty_string_to_null('patient_id') }} as subscriber_id
+  ,{{ empty_string_to_null('relation') }} as relationship
+  ,{{ empty_string_to_null('lob') }} as network
+  ,{{ empty_string_to_null('insurance_name') }} as payor
+  ,{{ empty_string_to_null('client_id') }} as client_id
+  ,ingest_date
+from eligibility
+where row_num = 1
