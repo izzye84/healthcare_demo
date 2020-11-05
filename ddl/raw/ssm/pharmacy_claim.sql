1d68f7363fd0b22ca5c683065e911bc9589e0115
killian-dent <64610812+killian-dent@users.noreply.github.com>
2020-11-05 10:53:00 -0700

    ddl fix

--- ddl/raw/ssm/pharmacy_claim.sql
+++ ddl/raw/ssm/pharmacy_claim.sql
@@ -10,13 +10,13 @@ create external table raw_ssm.pharmacy_claim (
     date_of_birth date,
     ndc_code varchar(255),
     days_supply int,
-    fill_date_time timestamp,
+    fill_date_time varchar(255),
     claim_adjustment_type_code varchar(255),
     claim_adjustment_sequence_number varchar(255),
-    claim_adjustment_date_time timestamp,
+    claim_adjustment_date_time varchar(255),
     allowed_amount numeric(18,2),
     charge_amount numeric(18,2),
-    payment_date_time timestamp,
+    payment_date_time varchar(255),
     plan_payment_amount numeric(18,2),
     member_payment_amount numeric(18,2),
     daw varchar(255),
@@ -34,11 +34,11 @@ partitioned by (client_id varchar(50), lob varchar(50), insurance_name varchar(5
 row format delimited
 fields terminated by '|'
 stored as textfile
-location 's3://strive-analytics-warehouse-pro/clients/client_id=ssm/data_frequency=batch/pharmacy_claim/'
+location 's3://strive-analytics-warehouse-pro/clients/client_id=ssm/data_frequency=batch/pharmacy_claims/'
 table properties ('skip.header.line.count' = '1');
 
 /* The following statement is an example of what needs to be run for each partition to ensure all data is loaded into the external table */
 
 alter table raw_ssm.pharmacy_claim add
 partition(client_id='ssm', lob='Medicare FFS', insurance_name='MSSP Missouri', ingest_date='2020-03-06') 
-location 's3://strive-analytics-warehouse-pro/clients/client_id=ssm/data_frequency=batch/pharmacy_claim/lob=Medicare_FFS/insurance_name=MSSP_Missouri/ingest_date=2020-03-06/';
+location 's3://strive-analytics-warehouse-pro/clients/client_id=ssm/data_frequency=batch/pharmacy_claims/lob=Medicare_FFS/insurance_name=MSSP_Missouri/ingest_date=2020-03-06/';
