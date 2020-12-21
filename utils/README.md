d0b1190fb6db80063c08fa1cad54ea27da42b15c
Dan Wallace <dwallace@strivehealth.com>
2020-12-21 13:54:37 -0800

    Added utils directory with file copying and partition creation Python scripts.

---
+++ utils/README.md
@@ -0,0 +1,43 @@
+# Utils
+
+## s3_file_copying
+
+All customer files are copied from Platform client service buckets (ex. strive-ssm-service-data-pro) to the Analytics Warehouse's (AW) bucket. Partitions are then added to the raw AW tables.
+
+An example partition query:
+
+```
+alter table raw_ssm.vitals add if not exists partition(client_id='ssm', ingest_date='2020-10-09') location 's3://strive-analytics-warehouse-pro/clients/client_id=ssm/data_frequency=batch/vitals/ingest_date=2020-10-09/';
+```
+
+### Usage
+
+1. Log into an AWS SSO account that has permission to access AWS Secrets Manager.
+2. `python3 copy_new_files.py`
+3. `python3 add_partitions.py`
+
+### analytics_warehouse.py
+
+This is a Python class for connecting to the Analytics Warehouse Redshift cluster and running queries. It uses the Python package [psycopg2](https://www.psycopg.org/docs/).
+
+### copy_new_files.py
+
+This Python script copies files from the client services into the AW bucket if they have not been previously ingested.
+
+#### Adding new customers and file types
+
+This script currently requires quite a bit of manual work to add new customers.
+
+The most important part of the script is the mapping from source to destination. This can be found in the section:
+
+```
+#####################################################
+#   Mapping from Source Path to Destination Path    #
+#####################################################
+```
+
+Otherwise, look through the script for examples of how SSM and Humana are handled. As we add more customers we can simplify this process, but it will semi-tedious as long as clients provide different file types with different schemas. 
+
+### add_partitions.py
+
+This Python script creates the partitions in the AW's raw tables. 
---
+++ utils/s3_file_copying/add_partitions.py
@@ -0,0 +1,94 @@
+import time
+import json
+import logging
+import re
+from typing import List, Optional, Tuple
+
+import boto3
+import psycopg2
+
+from analytics_warehouse import AnalyticsWarehouse
+
+import os
+if not os.path.exists('.file_copying_logs'):
+    os.makedirs('.file_copying_logs')
+
+
+FORMAT = '[%(asctime)-15s](%(name)s) %(message)s'
+LOGGING_FILENAME = f'.file_copying_logs/add_partitions_{int(time.time())}.log'
+logging.basicConfig(filename=LOGGING_FILENAME, level=logging.DEBUG, format=FORMAT)
+
+aw = AnalyticsWarehouse(dbname='strive-prod', secret_name='AnalyticsWarehouseDataLoadService')
+
+print('Adding partitions to Redshift Spectrum external tables...')
+
+key_with_lob_pattern = re.compile(r'(clients/client_id=(\w+)/data_frequency=\w+/(\w+)/lob=(\w+)/insurance_name=(\w+)/ingest_date=(\d+-\d+-\d+)/)')
+key_no_lob_pattern = re.compile(r'(clients/client_id=(\w+)/data_frequency=\w+/(\w+)/ingest_date=(\d+-\d+-\d+)/)')
+
+aw_bucket_name = 'strive-analytics-warehouse-pro'
+s3 = boto3.resource('s3')
+aw_bucket = s3.Bucket(aw_bucket_name)
+
+objs = aw_bucket.objects.filter(Prefix='clients/')
+keys = [k.key for k in list(objs) if 'conviva' not in k.key]
+
+# A set of prefixes that have already been added
+prefixes = set()
+
+for k in keys:
+    m_lob = re.search(key_with_lob_pattern, k)
+    if m_lob:
+        prefix = m_lob.group(1)
+        if prefix not in prefixes:
+            prefixes.add(prefix)
+            client_id = m_lob.group(2)
+            file_type = m_lob.group(3)
+            lob = m_lob.group(4)
+            lob = lob.replace('_', ' ')
+            insurance_name = m_lob.group(5)
+            insurance_name = insurance_name.replace('_', ' ')
+            ingest_date = m_lob.group(6)
+            table_name = f'raw_{client_id}.{file_type}'
+            logging.debug(
+                (
+                    f'Adding partition for table_name={table_name}, '
+                    f'prefix={prefix}, client_id={client_id}, '
+                    f'ingest_date={ingest_date}, lob={lob}, insurance_name={insurance_name}'
+                )
+            )
+            aw.add_partition(
+                table_name,
+                prefix,
+                client_id,
+                ingest_date,
+                lob,
+                insurance_name
+            )
+    m_no_lob = re.search(key_no_lob_pattern, k)
+    if m_no_lob:
+        prefix = m_no_lob.group(1)
+        if prefix not in prefixes:
+            prefixes.add(prefix)
+            client_id = m_no_lob.group(2)
+            file_type = m_no_lob.group(3)
+            ingest_date = m_no_lob.group(4)
+            table_name = f'raw_{client_id}.{file_type}'
+            # Temp filter for pharmacy_claims file that breaks the schema !!!
+            if 'pharmacy_claims/ingest_date=2020-06-15' in prefix:
+                continue
+            logging.debug(
+                (
+                    f'Adding partition for table_name={table_name}, '
+                    f'prefix={prefix}, client_id={client_id}, '
+                    f'ingest_date={ingest_date}'
+                )
+            )
+            aw.add_partition(
+                table_name,
+                prefix,
+                client_id,
+                ingest_date
+            )
+
+print('Finished adding partitions')
+print(f'Logs written to {LOGGING_FILENAME}')
