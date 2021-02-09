ee8f5d4fbff5f4a21071fdefcd487d81fbfd2336
Izzy Erekson <ierekson@Izzys-MacBook-Pro.local>
2021-02-08 23:13:30 -0700

    staging transformation step and tests for humana's medical claims to claim line

---
+++ models/staging/humana/stg_humana_common__claim_line.sql
@@ -0,0 +1,43 @@
+with medical_claims as (
+
+    select *
+    from {{ ref('base_humana_common__medical_claims') }}
+
+),
+
+claim_line as (
+
+    select {{ dbt_utils.surrogate_key(['line_number',
+                                       'client_id']) }} as sh_line_number,
+           claim_id,
+           row_number() over (partition by claim_id
+                              order by line_number) as line_number,
+           member_id,
+           null as patient_id,
+           billing_provider_npi,
+           null as servicing_provider_npi,
+           revenue_code,
+           procedure_code,
+           procedure_modifier_1,
+           procedure_modifier_2,
+           null as procedure_modifier_3,
+           null as procedure_modifier_4,
+           ndc_code,
+           serv_from_date as service_date_from,
+           serv_to_date as service_date_to,
+           service_units,
+           total_charges as charge_amount,
+           member_liability + plan_payment as total_paid,
+           allowed_amount,
+           plan_payment,
+           member_liability,
+           null as standard_cost,
+           'Facility' as claim_type,
+           transaction_type
+
+    from medical_claims
+
+)
+
+select *
+from claim_line
